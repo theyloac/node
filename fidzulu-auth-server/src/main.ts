eb4f3e6d9c13e862c1caea0e494079518ea35911
theyloac <theyloac@gmail.com>
2026-02-28 09:48:01 -0500

    add the global /api/v1 prefix to all routes

--- fidzulu-auth-server/src/main.ts
+++ fidzulu-auth-server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1'); // adds /api/v1 prefix to all routes (required)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
