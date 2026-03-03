e16611961aa467ae2817ce865191c46079b96fc1
theyloac <theyloac@gmail.com>
2026-03-03 08:46:27 -0500

    fix: better formating user dto

--- fidzulu-auth-server/src/users/dto/get-user.dto.ts
+++ fidzulu-auth-server/src/users/dto/get-user.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class GetUserDto {
-  @ApiProperty({ description: 'User id' })
-  id: number;
+    @ApiProperty({ description: 'First name of the user' })
+    firstname: string;
 
-  @ApiProperty({ description: 'Username' })
-  username?: string;
+    @ApiProperty({ description: 'Last name of the user' })
+    lastname: string;
 
-  @ApiProperty({ description: 'Display name' })
-  displayName?: string;
+    @ApiProperty({ description: 'Username' })
+    username: string;
+
+    @ApiProperty({ description: 'Email address' })
+    email: string;
+
+    @ApiProperty({ description: 'User role e.g. USER, ADMIN, GUEST' })
+    role: string;
 }
